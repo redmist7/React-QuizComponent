0dc42181fea59dd3c0fbf14eae5bd7d9d9c70bb5
m1050954 <m1050954@mindtree.com>
2020-11-12 17:13:09 +0530

    updated

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   handleClick = (buttonText) => {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   handleClick = () => {
     this.props.clickHandler(this.props.button_text);
   };
