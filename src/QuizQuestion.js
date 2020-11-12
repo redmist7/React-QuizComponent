ee1039fa7d4e34d2a6817853f99eacd65740c043
m1050954 <m1050954@mindtree.com>
2020-11-12 17:23:19 +0530

    updated

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState({
+        incorrectAnswer: false,
+      });
+    } else {
+      this.setState({
+        incorrectAnswer: true,
+      });
     }
-  };
+  }
   render() {
     return (
       <main>
@@ -25,6 +38,9 @@ class QuizQuestion extends Component {
               );
             })}
           </ul>
+          {this.state.incorrectAnswer ? (
+            <p className='error'>Sorry, that's not right</p>
+          ) : null}
         </section>
       </main>
     );
